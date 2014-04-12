a47b0a207b54ecf4331f9aa29eceb9f7c15fc290
Dan <dan.talpau@gmail.com>
2014-04-12 23:54:58 +0100

    add YAML support

--- src/main/java/com/github/dgt79/App.java
+++ src/main/java/com/github/dgt79/App.java
@@ -1,11 +1,21 @@
 package com.github.dgt79;
 
 import org.eclipse.jetty.server.Server;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Map;
 
 public class App {
-	public static void main(String... args) throws Exception {
+	final static String PORT = "port";
 
-		Server server = new Server(8080);
+	public static void main(String... args) throws Exception {
+		Yaml yamlConfig = new Yaml();
+		final Map config = (Map<String, String>) yamlConfig.load(new FileInputStream(new File(".config.yml")));
+
+
+		Server server = new Server((Integer) config.get(PORT));
 
 
 		server.start();
